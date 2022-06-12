d2a95dddcc399775c47b140ef68df9a0dc1d20b6
Fabiano Parente <fabiano@nunes.me>
2022-06-11 23:26:11 -0300

    inicial

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+FROM ubuntu:20.04 as openfortivpn
+
+ARG OPENFORTIVPN_URL=https://github.com/adrienverge/openfortivpn/archive/refs/tags/v1.17.3.tar.gz
+COPY ./patches /patches
+RUN set -ex;                                 \
+  apt-get update;                            \
+  apt-get install -y --no-install-recommends \
+    autoconf                                 \
+    automake                                 \
+    ca-certificates                          \
+    gcc                                      \
+    libc6-dev                                \
+    libssl-dev                               \
+    make                                     \
+    patch                                    \
+    pkg-config                               \
+    wget;                                    \
+  wget "$OPENFORTIVPN_URL";                  \
+  tar -xzf v1.17.3.tar.gz;                   \
+  cd openfortivpn-1.17.3;                    \
+  cat /patches/* | patch -p1;                \
+  ./autogen.sh;                              \
+  ./configure;                               \
+  make;
+
+FROM ubuntu:20.04
+
+RUN set -ex;                                 \
+  apt-get update;                            \
+  apt-get install -y --no-install-recommends \
+    ca-certificates                          \
+    openssl                                  \
+    ppp;                                     \
+  rm -rf /var/lib/apt/lists/*;
+
+COPY --from=openfortivpn /openfortivpn-1.17.3/openfortivpn /usr/bin/openfortivpn
+ENTRYPOINT ["openfortivpn", "-c", "/vpn/config.cfg", "--svpn-cookie", "-"]
---
+++ autentica.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+import signal
+import time
+from os import path
+import argparse
+import gi
+
+gi.require_version("Gtk", "3.0")
+gi.require_version("WebKit2", "4.0")
+from gi.repository import Gtk, WebKit2
+
+COOKIE_NAME = "SVPNCOOKIE"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--url', required=True)
+args = parser.parse_args()
+
+class Browser:
+    window = None
+    cookie_manager = None
+
+    def run(self):
+        Gtk.init(None)
+        self.window = Gtk.Window()
+        self.window.set_title("Login")
+        self.window.connect("delete-event", Gtk.main_quit)
+        self.window.resize(475, 680)
+
+        ctx = WebKit2.WebContext.get_default()
+        cookie_storage_path = path.expanduser("~/.cache/openfortivpn-2fa")
+
+        self.cookie_manager = ctx.get_cookie_manager()
+        self.cookie_manager.set_accept_policy(WebKit2.CookieAcceptPolicy.ALWAYS)
+        self.cookie_manager.set_persistent_storage(
+            cookie_storage_path, WebKit2.CookiePersistentStorage.TEXT
+        )
+
+        web_view = WebKit2.WebView()
+        web_view.set_zoom_level(1.2)
+        web_view.connect("load-changed", self.page_changed)
+        web_view.load_uri(args.url)
+
+        self.window.add(web_view)
+
+        signal.signal(signal.SIGALRM, self.show)
+        signal.alarm(3)
+
+    def show(self, _signun, _stack):
+        self.window.show_all()
+
+    def page_changed(self, webview, event=None):
+        if event != WebKit2.LoadEvent.FINISHED:
+            return
+        mr = webview.get_main_resource()
+        uri = mr.get_uri()
+        self.cookie_manager.get_cookies(uri, None, on_get_cookie)
+
+
+def on_get_cookie(cookie_manager, result):
+    f = cookie_manager.get_cookies_finish(result)
+    cookies = {c.name: c.value for c in f}
+    if COOKIE_NAME in cookies:
+        print("{}={}".format(COOKIE_NAME, cookies[COOKIE_NAME]))
+        Gtk.main_quit()
+
+
+if __name__ == "__main__":
+    start = time.time()
+    Browser().run()
+    Gtk.main()
